4f6a754121d17126084c4cb13b81af7f71fda55f
Juri Saltbacka <juri.saltbacka@zoan.fi>
2021-07-24 18:01:41 +0300

    added ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,22 @@
+import ReactGA from "react-ga";
+import { useRouter } from "next/router";
+
+ReactGA.initialize("G-3BEQG6TMQ0");
+
 import "../styles/styles.css";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ReactGA.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
