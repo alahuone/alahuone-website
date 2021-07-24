3e3d405cdb46e828973e8258a6bb1ea18f319d1d
Juri Saltbacka <juri.saltbacka@zoan.fi>
2021-07-24 10:06:32 +0300

    added some appropriate styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
+import Link from "next/link";
 import Head from "next/head";
 
 export default function IndexPage() {
   return (
     <>
       <Head>
-        <title>alahuone.co</title>
+        <title>alahuone</title>
       </Head>
-      <h1>alahuone.co</h1>
+      <h2>club</h2>
+      <h1>alahuone</h1>
+      <div className="right">
+        <a href="https://discord.gg/awHmJ9UDxr">your personal invitation 👈</a>
+        <br />
+        <a href="mailto:hello@alahuone.co">
+          <small>or get in touch 💋</small>
+        </a>
+      </div>
     </>
   );
 }
